ae6c005670b3bc1279389a2e208dacc699d26bed
Vijay <vijayshinde@movingtrumpet.com>
2017-08-01 16:41:23 +0530

    added task4

--- calculator.php
+++ calculator.php
@@ -1,15 +1,20 @@
 <?php
 // check if argument pass or not
 if (count($argv) <= 1){ 
-echo "Please pass valid arguments"; 
-exit;
+$result = "Please pass valid arguments"; 
 }
 
 //if passed valid argument then sum the numbers
 if($argv[1] === 'sum'|| $argv[1] === 'add'){
 	if(isset($argv[2])){
-		//take \n as number separator
-		$numArr = explode(",", str_replace("n", ',', $argv[2]));
+
+		$numArr = explode("\\",$argv[2]);
+		if(count($numArr) == 1){
+			//take \n as number separator
+			$numArr = explode(",", str_replace("n", ',', $argv[2]));
+		}else{
+			$numArr = explode($numArr[1], str_replace("n", ',', $numArr[2]));
+		}
 		//sum all value of array
 		$result = array_sum($numArr);
 	}else{
