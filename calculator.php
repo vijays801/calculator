d9972d23252a5167b21a4ea15a14bc1df26c4053
Vijay <vijayshinde@movingtrumpet.com>
2017-08-01 17:56:26 +0530

    Added task8

--- calculator.php
+++ calculator.php
@@ -5,7 +5,7 @@ $result = "Please pass valid arguments";
 }
 
 //if passed valid argument then sum the numbers
-if($argv[1] === 'sum'|| $argv[1] === 'add'){
+if($argv[1] === 'sum'|| $argv[1] === 'add' || $argv[1] === 'multiply'){
 	if(isset($argv[2])){
 
 		$numArr = explode("\\",$argv[2]);
@@ -22,39 +22,19 @@ if($argv[1] === 'sum'|| $argv[1] === 'add'){
 		}else{
 			$numArr = array_filter($numArr, 'isSmaller');
 
-			//sum all value of array
-			$result = array_sum($numArr);
+			if($argv[1] !== 'multiply'){
+				//sum all value of array
+				$result = array_sum($numArr);
+			}else{
+				//get product of all values of the array
+				$result = array_product ($numArr);
+			}
 		}
 	}else{
 		$result = 0;
 	}
 }
 
-//if passed valid argument then multiply the numbers
-if($argv[1] === 'multiply'){
-	if(isset($argv[2])){
-
-		$numArr = explode("\\",$argv[2]);
-		if(count($numArr) == 1){
-			//take \n as number separator
-			$numArr = explode(",", str_replace("n", ',', $argv[2]));
-		}else{
-			$numArr = explode($numArr[1], str_replace("n", ',', $numArr[2]));
-		}
-
-		$arr = array_filter($numArr, 'isnegative');
-		if(count($arr)>0){
-			$result = "Error: Negative numbers(".implode(",", $arr).") not allowed.";
-		}else{
-			$numArr = array_filter($numArr, 'isSmaller');
-
-			//get product of all values of the array
-			$result = array_product ($numArr);
-		}
-	}else{
-		$result = 0;
-	}
-}
 
 echo $result;
 
